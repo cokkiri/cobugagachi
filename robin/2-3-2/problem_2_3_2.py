21ecbae390bf0d27f6b7d0bb65fc02d38effd300
robin <er7812@naver.com>
2021-03-23 22:58:25 +0900

    apply advices by dobro

--- robin/2-3-2/problem_2_3_2.py
+++ robin/2-3-2/problem_2_3_2.py
@@ -5,17 +5,16 @@ with open('./input.txt') as fp:
     lines = fp.readlines()
 
 N, M, K = map(int, lines[0].split(' '))
-assert 2<=N<=1e3 and 1<=M<=1e4 and 1<=K<=1e5
-assert K<=M
+assert (2 <= N <= 1e3) and (1 <= M <= 1e4) and (1 <= K <= 1e5) and (K <= M), 'N, M, K = {}, {}, {}'.format(N, M, K)
 
 nums = map(int, lines[1].split(' '))
-nums.sort(reverse=True)
-assert len(nums)==N
+sorted_nums = sorted(nums, reverse=True)
+assert len(sorted_nums) == N, 'len = {}, N = {}'.format(len(sorted_nums), N)
 
 cnt = 0
 sum = 0
 brk_flag = False
-num1, num2 = nums[:2]
+num1, num2 = sorted_nums[:2]
 
 while not brk_flag:
     for idx in range(K):
