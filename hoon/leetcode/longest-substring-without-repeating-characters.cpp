1cb6c69eca35972e607f8c48fb067f74bcdeedaa
hoonseokkim <hoonseok.dev@gmail.com>
2021-03-26 21:25:13 +0900

    [leetcode] median-of-two-sorted-arrays in cpp - https://leetcode.com/problems/median-of-two-sorted-arrays

--- hoon/leetcode/longest-substring-without-repeating-characters.cpp
+++ hoon/leetcode/longest-substring-without-repeating-characters.cpp
@@ -1,37 +1,40 @@
-class Solution {
+#include <iostream>
+#include <string>
+using namespace std;
+class Solution
+{
 public:
-    int lengthOfLongestSubstring(string s) {
+    int lengthOfLongestSubstring(string s)
+    {
         string substring = "";
         string currentsSubstring = "";
-        for (int i=0; i<s.size(); i++)
+        for (int i = 0; i < s.size(); i++)
         {
-            int j=0;
-            for(j=0; j<currentsSubstring.size(); j++)
+            int j = 0;
+            for (j = 0; j < currentsSubstring.size(); j++)
             {
                 if (currentsSubstring[j] == s[i])
                 {
                     if (substring.size() < currentsSubstring.size())
                         substring = currentsSubstring;
-                    
+
                     break;
                 }
-                    
             }
-            
-            if (j<currentsSubstring.size())
+
+            if (j < currentsSubstring.size())
             {
-                currentsSubstring = currentsSubstring.substr(j+1, currentsSubstring.size()) + s[i];
+                currentsSubstring = currentsSubstring.substr(j + 1, currentsSubstring.size()) + s[i];
             }
             else
             {
-                currentsSubstring += s[i];     
+                currentsSubstring += s[i];
             }
         }
-        
+
         if (substring.size() < currentsSubstring.size())
             substring = currentsSubstring;
-        
+
         return substring.size();
-        
     }
 };
